623afce38936f059f77919df850a0a9dddad75e9
Kaveesha Gimhana <36946467+KaveeshaG@users.noreply.github.com>
2023-11-20 00:59:29 +0530

    feat: added Dockerfile

--- src/models/books.model.js
+++ src/models/books.model.js
@@ -1,6 +1,6 @@
 let books = [
     { id: 1, title: 'Book 1', author: 'Author 1' },
-    { id: 2, title: 'Book 2', author: 'Author 2' },
+    { id: 2, title: 'Book 2', author: 'Lihini' },
     { id: 3, title: 'Book 3', author: 'Author 3' }
 ];
 
